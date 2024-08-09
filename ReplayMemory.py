c22fe92f44466eaef9c006fe2a81b7b6f5a1710e
nklsdhbw <nklsdhbw@gmx.de>
2024-08-10 00:58:03 +0200

    added type hints for ReplayMemory.py

--- ReplayMemory.py
+++ ReplayMemory.py
@@ -2,20 +2,21 @@ import torch
 from collections import namedtuple, deque
 import random
 import numpy as np
+from typing import Tuple, Deque, NamedTuple
 
 class ReplayMemory:
-    def __init__(self, action_size, buffer_size, batch_size, seed):
-        self.action_size = action_size
-        self.memory = deque(maxlen=buffer_size)
-        self.batch_size = batch_size
+    def __init__(self, action_size: int, buffer_size: int, batch_size: int, seed: int) -> None:
+        self.action_size: int = action_size
+        self.memory: Deque[NamedTuple] = deque(maxlen=buffer_size)
+        self.batch_size: int = batch_size
         self.experience = namedtuple("Experience", field_names=["state", "action", "reward", "next_state", "done"])
-        self.seed = random.seed(seed)
+        self.seed: None = random.seed(seed)
 
-    def add(self, state, action, reward, next_state, done):
+    def add(self, state: np.ndarray, action: int, reward: float, next_state: np.ndarray, done: bool) -> None:
         e = self.experience(state, action, reward, next_state, done)
         self.memory.append(e)
 
-    def sample(self, device):
+    def sample(self, device: torch.device) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]:
         experiences = random.sample(self.memory, k=self.batch_size)
         states = torch.from_numpy(np.vstack([e.state for e in experiences if e is not None])).float().to(device)
         actions = torch.from_numpy(np.vstack([e.action for e in experiences if e is not None])).long().to(device)
@@ -24,5 +25,5 @@ class ReplayMemory:
         dones = torch.from_numpy(np.vstack([e.done for e in experiences if e is not None]).astype(np.uint8)).float().to(device)
         return (states, actions, rewards, next_states, dones)
 
-    def __len__(self):
+    def __len__(self) -> int:
         return len(self.memory)
